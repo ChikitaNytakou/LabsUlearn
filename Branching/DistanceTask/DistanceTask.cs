b8a4efe02055992a16d880cf4ff5dd92f634ddb8
chikita <chikita.nytakou@gmail.com>
2024-12-12 23:42:34 +0300

    Solved Task DistanceTask

--- Branching/DistanceTask/DistanceTask.cs
+++ Branching/DistanceTask/DistanceTask.cs
@@ -7,6 +7,11 @@ public static class DistanceTask
     // Расстояние от точки (x, y) до отрезка AB с координатами A(ax, ay), B(bx, by)
     public static double GetDistanceToSegment(double ax, double ay, double bx, double by, double x, double y)
     {
-        return 0.0;
+        if (x == y & ax == bx & ay == by & x == ax & y == by) return 0;
+        if (ax == bx & ay == by) return Math.Sqrt(Math.Pow(ax - x, 2) + Math.Pow(ay - y, 2));
+        double t = ((x - ax) * (bx - ax) + (y-ay) * (by - ay)) / (Math.Pow(bx - ax, 2) + Math.Pow(by - ay, 2));
+        if (t < 0) t = 0;
+        if (t > 1) t = 1;
+        return Math.Sqrt(Math.Pow(ax - x + (bx - ax) * t, 2) + Math.Pow(ay - y + (by - ay) * t, 2));
     }
 }
