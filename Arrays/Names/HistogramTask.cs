cd852c32559c8798e37435098f651f7860d84015
chikita <chikita.nytakou@gmail.com>
2024-12-15 21:37:26 +0300

    Solved problem merging

--- Arrays/Names/HistogramTask.cs
+++ Arrays/Names/HistogramTask.cs
@@ -7,13 +7,9 @@ internal static class HistogramTask
     public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
     {
         var days = new string[31];
-<<<<<<< HEAD
-        for (int i = 0; i < days.Length; i++) {
-=======
         for (int i = 0; i < days.Length; i++)
         {
->>>>>>> branchFromNames
-            days[i] = (i + 1).ToString();
+           days[i] = (i + 1).ToString();
         }
         var birthsCounts = new double[31];
         for (int i = 2; i <= birthsCounts.Length; i++)
