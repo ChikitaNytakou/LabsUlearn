f1d204ac172eae429d8aaae3bee87d224b0eded4
chikita <chikita.nytakou@gmail.com>
2024-12-15 21:12:52 +0300

    Solved Task Names in branchFromNames

--- Arrays/Names/HistogramTask.cs
+++ Arrays/Names/HistogramTask.cs
@@ -6,9 +6,11 @@ internal static class HistogramTask
 {
     public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
     {
-        var days = new string[] {"1","2","3","4","5","6","7","8","9","10","11",
-                    "12","13","14","15","16","17","18","19","20","21",
-                    "22","23","24","25","26","27","28","29","30","31"};
+        var days = new string[31];
+        for (int i = 0; i < days.Length; i++)
+        {
+            days[i] = (i + 1).ToString();
+        }
         var birthsCounts = new double[31];
         for (int i = 2; i <= birthsCounts.Length; i++)
         {
