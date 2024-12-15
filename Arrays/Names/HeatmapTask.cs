27117d4301d04de20410409786645026fce99be8
chikita <chikita.nytakou@gmail.com>
2024-12-15 21:00:17 +0300

    Solved Task Names v2

--- Arrays/Names/HeatmapTask.cs
+++ Arrays/Names/HeatmapTask.cs
@@ -1,13 +1,29 @@
-﻿namespace Names;
+﻿using System.Xml.Linq;
+
+namespace Names;
 
 internal static class HeatmapTask
 {
     public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
     {
+        var days = new string[30];
+        var months = new string[12];
+        double[,] birthsCounts = new double[days.Length, months.Length];
+        for (int i = 0; i < days.Length; i++)
+        {
+            days[i] = (i + 2).ToString();
+            if (i < 12)
+                months[i] = (i + 1).ToString();
+        }
+        for (int i = 0; i < names.Length; i++)
+        {
+            if (names[i].BirthDate.Day == 1) continue;
+            birthsCounts[names[i].BirthDate.Day - 2, names[i].BirthDate.Month - 1]++;
+        }
         return new HeatmapData(
-            "Пример карты интенсивностей",
-            new double[,] { { 1, 2, 3 }, { 2, 3, 4 }, { 3, 4, 4 }, { 4, 4, 4 } }, 
-            new[] { "a", "b", "c", "d" }, 
-            new[] { "X", "Y", "Z" });
+            "Тепловая карта рождаемости в зависимости от дня и месяца",
+            birthsCounts,
+            days,
+            months);
     }
 }
