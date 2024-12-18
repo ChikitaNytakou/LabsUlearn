cb34df1d270d54e6b621dd47e6527e9ad339149c
chikita <chikita.nytakou@gmail.com>
2024-12-18 22:44:22 +0300

    Added NUnit Tests For Testing

---
+++ SolverNUnitTests/SolverNUnitTest.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using static TableParser.Solver;
+
+namespace SolverNUnitTests
+{
+    [TestFixture]
+    public class SolverNUnitTest
+    {
+        [Test]
+        [TestCase(5, 2, 3, Operation.Add)]
+        [TestCase(-1, 2, -3, Operation.Add)]
+        [TestCase(1, -2, 3, Operation.Add)]
+        [TestCase(2, 2, 0, Operation.Add)]
+        public void TestOperationAdd(double expected, double x, double y, Operation operation)
+        {
+            ClassicAssert.AreEqual(expected, Solve(x, y, operation));
+        }
+
+        [Test]
+        [TestCase(-1, 2, 3, Operation.Subtract)]
+        [TestCase(1, 4, 3, Operation.Subtract)]
+        [TestCase(2, 2, 0, Operation.Subtract)]
+        [TestCase(-2, 0, 2, Operation.Subtract)]
+        [TestCase(0, 0, 0, Operation.Subtract)]
+        [TestCase(5, 2, -3, Operation.Subtract)]
+        [TestCase(1, -2, -3, Operation.Subtract)]
+        public void TestOperationSubtract(double expected, double x, double y, Operation operation)
+        {
+            ClassicAssert.AreEqual(expected, Solve(x, y, operation));
+        }
+
+        [Test]
+        [TestCase(6, 2, 3, Operation.Multiply)]
+        [TestCase(0, 2, 0, Operation.Multiply)]
+        [TestCase(-4, 2, -2, Operation.Multiply)]
+        [TestCase(4, -2, -2, Operation.Multiply)]
+        public void TestOperationMultiply(double expected, double x, double y, Operation operation)
+        {
+            ClassicAssert.AreEqual(expected, Solve(x, y, operation));
+        }
+
+        [Test]
+        [TestCase(1, 3, 3, Operation.Divide)]
+        [TestCase(double.PositiveInfinity, 3, 0, Operation.Divide)]
+        [TestCase(double.NaN, 0, 0, Operation.Divide)]
+        [TestCase(0, 0, 3, Operation.Divide)]
+        public void TestOperationDivide(double expected, double x, double y, Operation operation)
+        {
+            ClassicAssert.AreEqual(expected, Solve(x, y, operation));
+        }
+    }
+}
