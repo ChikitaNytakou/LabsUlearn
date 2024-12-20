f27518724d0facdfc915821248381a5a805e0040
chikita <chikita.nytakou@gmail.com>
2024-12-20 16:16:20 +0300

    Solved 256-Gray Task

---
+++ Basic OOP/GeometryPainting/SegmentExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Avalonia.Media;
+using Geometry;
+
+//Напишите здесь код, который заставит работать методы segment.GetColor и segment.SetColor
+
+namespace GeometryPainting
+{
+    public static class SegmentExtensions
+    {
+        public static Dictionary<Segment, Color> dicionary = new Dictionary<Segment, Color>();
+        public static Color GetColor(this Segment segment)
+        {
+            if (!dicionary.ContainsKey(segment)) return Colors.Black;
+            else return dicionary[segment];
+        }
+
+        public static void SetColor(this Segment segment, Color color)
+        {
+            dicionary[segment] = color;
+        }
+    }
+}
+
