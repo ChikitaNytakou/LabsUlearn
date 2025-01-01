1da52e59d2e5598959369f5ce9ff326efba8da51
chikita <chikita.nytakou@gmail.com>
2025-01-01 19:33:45 +0300

    P.2 L.1 added lazy collections

---
+++ Lessons/P.2 L.1/Lazy Collections/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+
+namespace Lazy_Collections
+{
+    public class Sequences
+    {
+        public static IEnumerable<long> Fibonacci
+        {
+            get
+            {
+                long previous = 1;
+                long current = 1;
+                yield return 1;
+                yield return 1;
+                while (true)
+                {
+                    var newValue = previous + current;
+                    previous = current;
+                    current = newValue;
+                    yield return current;
+                }
+
+            }
+        }
+
+        public static IEnumerable<long> Exponential(int multiplier)
+        {
+            var value = 1;
+            while (true)
+            {
+                yield return value;
+                value *= multiplier;
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var e in Sequences.Exponential(2))
+            {
+                Console.WriteLine(e);
+                Thread.Sleep(100);
+                if (Console.KeyAvailable) break;
+            }
+        }
+    }
+}
